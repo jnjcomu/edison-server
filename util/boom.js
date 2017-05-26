8882af5b2af0e9e8d0900816bacaf444250cd622
ChalkPE <chalk@chalk.pe>
2017-05-26 09:55:04 +0900

    Ignore 4xx errors

--- util/boom.js
+++ util/boom.js
@@ -4,10 +4,11 @@ module.exports = () => async (ctx, next) => {
   try {
     await next()
   } catch (err) {
-    const { output } = Boom.wrap(err, err.statusCode)
+    const { output, isServer } = Boom.wrap(err, err.statusCode)
 
     ctx.body = output.payload
     ctx.status = output.statusCode
-    ctx.app.emit('error', err, ctx)
+
+    if (isServer) ctx.app.emit('error', err, ctx)
   }
 }
